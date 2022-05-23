c4ac67306a6333d2c2225d7e9a5fe32549977abd
priyaverma91 <105927698+priyaverma91@users.noreply.github.com>
2022-05-23 19:10:04 +0530

    Delete cypress directory

--- cypress/page-objects/header-page.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-export class HeaderPage {
-
-    verifyLogin(username) {
-        cy.get('a[title="Manage"]').should('contain.text', 'Hello '+username);
-    }
-
-}
